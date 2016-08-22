694db598ad9f70dc73c925388f81db1f6fe68ed9
jevontest <jevontest@gmail.com>
2016-08-22 17:17:13 +0800

    demo(IoC): p命名空间简化参数注入配置

--- spring-demo-ioc-nullvalue/src/main/java/org/spring/demo/ioc/nullvalue/App.java
+++ spring-demo-ioc-nullvalue/src/main/java/org/spring/demo/ioc/nullvalue/App.java
@@ -11,8 +11,12 @@ public class App
     public static void main( String[] args )
     {
 		ClassPathXmlApplicationContext application = new ClassPathXmlApplicationContext("spring-config.xml");
-		ExampleBean eb = application.getBean(ExampleBean.class);
+		ExampleBean eb = (ExampleBean) application.getBean("exampleBean");
 		System.out.println(eb.getEmail());
 		System.out.println(eb.getAddress());
+		ExampleBean classic = (ExampleBean) application.getBean("classic");
+		System.out.println(classic.getEmail());
+		ExampleBean p_namespace = (ExampleBean) application.getBean("p-namespace");
+		System.out.println(p_namespace.getEmail());
     }
 }
